0509a627374e66b4fbc589b7b538c9298aad6a65
voblak2 <vinceoblak@vinces-mbp.lan>
2020-03-03 22:40:40 -0600

    Changes to app

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+ //This is my first BST C# program
 namespace newApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int sum;
+            sum = 2 + 2;
+
+            int newSum;
+            newSum = sum + 10;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(newSum);  
         }
     }
 }
